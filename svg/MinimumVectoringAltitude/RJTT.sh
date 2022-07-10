6a9bab0969c07ee8c43932de597fe37a432895a9
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-11 00:18:18 +0900

    緯度経度を変数化

--- svg/MinimumVectoringAltitude/RJTT.sh
+++ svg/MinimumVectoringAltitude/RJTT.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+SKYTREE_N='354236N'
+SKYTREE_E='1394839E'
+
 cd `dirname $0`
 
 echo 'GEOMETRYCOLLECTION('
 
 echo 'LINESTRING('
-python ../../arc.py 354236N 1394839E 3 0 360 10
+python ../../arc.py $SKYTREE_N $SKYTREE_E 3 0 360 10
 echo ')'
 
 echo ')'
