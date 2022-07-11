64c76627f53a6108ffeb7d75891edef30d0f639e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-12 00:52:24 +0900

    交点を考慮に入れるなどした
    ほか、stepの細分化、それに伴う角度の小数点対応や、実行ディレクトリをリポジトリルートに変更した。

--- svg/MinimumVectoringAltitude/RJTT.sh
+++ svg/MinimumVectoringAltitude/RJTT.sh
@@ -6,11 +6,15 @@ SKYTREE_E='1394839E'
 DEFAULT_N='353312N'
 DEFAULT_E='1394652E'
 
+RJAA_ARP_N='354555N'
+RJAA_ARP_E='1402308E'
+
 MAG_VAR='-8'
 
-ARC_STEP=('5' '2' '1')
+ARC_STEP=('2' '1' '0.5')
 
 cd `dirname $0`
+cd '../../'
 
 echo 'GEOMETRYCOLLECTION('
 
@@ -25,10 +29,10 @@ arc() {
     echo ','
   fi
 
-  local ARC_FROM=`expr $1 + $MAG_VAR`
-  local ARC_TO=`expr $2 + $MAG_VAR`
+  local ARC_FROM=`echo "$1 + $MAG_VAR" | bc`
+  local ARC_TO=`echo "$2 + $MAG_VAR" | bc`
 
-  python -u ../../arc.py ${3:-$DEFAULT_N} ${4:-$DEFAULT_E} $RADIUS $ARC_FROM $ARC_TO ${ARC_STEP[ARC_STEP_LV]}
+  python -u arc.py ${3:-$DEFAULT_N} ${4:-$DEFAULT_E} $RADIUS $ARC_FROM $ARC_TO ${ARC_STEP[ARC_STEP_LV]}
 
   IS_FIRST=0
 }
@@ -38,8 +42,8 @@ arc() {
 # $3: lat [dms] (opt)
 # $4: lng [dms] (opt)
 arc_pt() {
-  local ANGLE=`expr $2 + $MAG_VAR`
-  python -u ../../point.py ${3:-$DEFAULT_N} ${4:-$DEFAULT_E} $1 $ANGLE
+  local ANGLE=`echo "$2 + $MAG_VAR" | bc`
+  python -u point.py ${3:-$DEFAULT_N} ${4:-$DEFAULT_E} $1 $ANGLE
 }
 # $1: bridge from [nm]
 # $2: bridge to [nm]
@@ -62,13 +66,42 @@ arc_bridge() {
 # $1: lng [dms] (opt)
 # $2: lat [dms] (opt)
 dms() {
-  python -u ../../toMapsui.py $1 $2
+  python -u toMapsui.py $1 $2
 }
 
 ARC_STEP_LV=0
 
 RADIUS='3'
-arc 0 360 $SKYTREE_N $SKYTREE_E
+arc 35 105 $SKYTREE_N $SKYTREE_E
+arc 110 186 $SKYTREE_N $SKYTREE_E
+arc 190 390 $SKYTREE_N $SKYTREE_E
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 30 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 30.221 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 35 $SKYTREE_N $SKYTREE_E
+echo ')'
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 105 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 106.015 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 110 $SKYTREE_N $SKYTREE_E
+echo ')'
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 186 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 189.81 $SKYTREE_N $SKYTREE_E
+echo ','
+arc_pt $RADIUS 190 $SKYTREE_N $SKYTREE_E
+echo ')'
 
 
 RADIUS='2'
@@ -90,9 +123,16 @@ arc 260 320
 
 
 RADIUS='10'
-arc 20 90
+arc 35 90
 arc 130 150
 arc 210 220
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 34.25146
+echo ','
+arc_pt $RADIUS 35
+echo ')'
 
 
 ARC_STEP_LV=1
@@ -151,12 +191,79 @@ arc 20 40
 
 
 RADIUS='80'
-arc 20 150
-arc 280 350 # TODO: crossing-check
-arc -40 70 354555N 1402308E # TODO: crossing-check
+arc 20 59
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 59
+echo ','
+dms '1410437.57' '362228.03'
+echo ','
+arc_pt $RADIUS 60
+echo ')'
+
+arc 60 150
+
+arc 280 356
+arc -26 29 $RJAA_ARP_N $RJAA_ARP_E
+arc 30 71 $RJAA_ARP_N $RJAA_ARP_E
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 356
+echo ','
+arc_pt $RADIUS 356.1735
+echo ','
+arc_pt $RADIUS -26 $RJAA_ARP_N $RJAA_ARP_E
+echo ')'
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 71 $RJAA_ARP_N $RJAA_ARP_E
+echo ','
+dms '1415132.38' '362134.18'
+echo ')'
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 29 $RJAA_ARP_N $RJAA_ARP_E
+echo ','
+arc_pt $RADIUS 29.1653 $RJAA_ARP_N $RJAA_ARP_E
+echo ','
+arc_pt $RADIUS 30 $RJAA_ARP_N $RJAA_ARP_E
+echo ')'
+
 
 RADIUS='105'
-arc 30 165 # TODO: crossing-check
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 42
+echo ','
+arc_pt $RADIUS 41.488
+echo ','
+arc_pt 80 29.1653 $RJAA_ARP_N $RJAA_ARP_E
+echo ')'
+
+arc 42 69.5
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 69.5
+echo ','
+dms '1414137.62' '362147.09'
+echo ','
+arc_pt $RADIUS 70.5
+echo ')'
+
+arc 70.5 156
+
+echo ','
+echo 'LINESTRING('
+arc_pt $RADIUS 156
+echo ','
+dms '1405156.08' '340242.55'
+echo ')'
 
 # Arc Bridges
 # from[nm] to[nm] angle[deg]
@@ -165,7 +272,25 @@ arc_bridge 2 7 200
 arc_bridge 2 3 260
 
 
-arc_bridge 3 15 20
+echo ','
+echo 'LINESTRING('
+arc_pt 3 20
+echo ','
+arc_pt 6.5 20
+echo ','
+arc_pt 3 189.81 $SKYTREE_N $SKYTREE_E
+echo ')'
+
+echo ','
+echo 'LINESTRING('
+arc_pt 15 20
+echo ','
+arc_pt 12.5 20
+echo ','
+arc_pt 3 30.221 $SKYTREE_N $SKYTREE_E
+echo ')'
+
+
 arc_bridge 3 10 50
 arc_bridge 3 15 320
 
@@ -190,7 +315,7 @@ arc_bridge 15 30 120
 arc_bridge 15 30 190
 
 
-arc_bridge 20 100 250 # TODO: crossing-check
+arc_bridge 20 90.04 250
 arc_bridge 20 80 280
 arc_bridge 20 80 330
 arc_bridge 20 80 330
@@ -199,12 +324,12 @@ arc_bridge 20 80 330
 arc_bridge 35 45 230
 
 
-arc_bridge 39 100 20 # TODO: crossing-check
+arc_bridge 39 94.2285 20
 arc_bridge 39 65 40
 
 
 arc_bridge 45 50 200
-arc_bridge 45 65 220 # Specified point available
+arc_bridge 45 65 220
 
 echo ','
 echo 'LINESTRING('
@@ -217,7 +342,14 @@ echo ')'
 
 
 arc_bridge 65 80 150
-arc_bridge 65 100 180 # TODO: crossing-check
+arc_bridge 65 80 180
+
+echo ','
+echo 'LINESTRING('
+echo `arc_pt 80 180`','
+echo `arc_pt 91.1224 180`''
+echo ')'
+
 
 
 arc_bridge 70 80 40
